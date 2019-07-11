a53e5d06cf45e2242558d84b3190b318a3241cee
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-07-11 15:37:02 -0700

    Created navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <h1>Portfolio</h1>
+        <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Nav(props) {
+  return (
+    <div className="nav">
+      <a>jeremy parker</a>
+      <a>about</a>
+      <a>sites</a>
+    </div>
+  )
+}
