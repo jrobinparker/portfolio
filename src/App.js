c98a5d0dc082163a3a81195f5d8da537e6eef9f1
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-07-12 10:42:38 -0700

    Created home page draft

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Nav from './components/Nav';
+import Body from './components/Body';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Nav />
+        <Body />
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Body extends React.Component {
+
+  render() {
+
+  return (
+    <div>
+    <div className="home-text">
+      <div className="welcome-text">
+        <div>i'm <span style={{ color: 'blue' }}>jeremy parker</span>,</div>
+        <div className="languages">an aspiring web developer from los angeles, ca</div>
+        <div className="links">
+          <a>about</a>
+          <a>sites</a>
+          <a>contact</a>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+}
+
+export default Body
