be76513bdb26ee079a09cbefdabe6f9ba677edd0
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2019-08-19 08:54:23 -0700

    Updates

---
+++ precache-manifest.bd23564904e86344c4169cadfd92bdcb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b504f618df93a290b8972f117a796b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "40e78177f656c8d01810",
+    "url": "/portfolio/static/css/main.357c6200.chunk.css"
+  },
+  {
+    "revision": "2fa2c408a34f258ffd46",
+    "url": "/portfolio/static/js/2.cb35dd7a.chunk.js"
+  },
+  {
+    "revision": "40e78177f656c8d01810",
+    "url": "/portfolio/static/js/main.224a0fb0.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5f320861bdfc53b8f4f36b9ef918ee19",
+    "url": "/portfolio/static/media/Jeremy Parker Resume.5f320861.pdf"
+  },
+  {
+    "revision": "779d8a9c35faac5733facf7b80517941",
+    "url": "/portfolio/static/media/Seascape.779d8a9c.svg"
+  },
+  {
+    "revision": "dbd214f8f1b177bfab1a3f4aa1c66da3",
+    "url": "/portfolio/static/media/box-bg.dbd214f8.png"
+  },
+  {
+    "revision": "2faf743ace9363d082c5c9330490f0b0",
+    "url": "/portfolio/static/media/diving.2faf743a.svg"
+  },
+  {
+    "revision": "5c2edae8e769e8c598ff169583d65a33",
+    "url": "/portfolio/static/media/jchain-gif.5c2edae8.gif"
+  },
+  {
+    "revision": "58c344381f6503ad55bc9500e763b26f",
+    "url": "/portfolio/static/media/jdict-gif.58c34438.gif"
+  },
+  {
+    "revision": "fe49cf6be702131730a1e2dec28f62d8",
+    "url": "/portfolio/static/media/night.fe49cf6b.png"
+  },
+  {
+    "revision": "16d1390c1bb0eaecef7dbbe60d34d622",
+    "url": "/portfolio/static/media/renshu-gif.16d1390c.gif"
+  }
+]);
