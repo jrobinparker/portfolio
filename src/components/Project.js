4997df97755bad764ef48aac7bf6ec9bddeb9ef7
Jeremy Parker <jrobinparker@gmail.com>
2020-02-20 10:42:35 -0800

    Updated styling for  Projects component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
 const Project = props => {
-  const { name, tech, about } = props
+  const { name, tech, about, github, img } = props
   return(
     <div className="project-item">
-        <div className="project-img">
-        </div>
-        <div className="project-about">
-          <div className="header subheader">{name}</div>
+      {img ? (
+        <React.Fragment>
+          <div className="project-img">
+            <img src={require(`../assets/${img}`)} />
+          </div>
+          <div className="project-about">
+            <div className="header subheader project-header">{name}</div>
+            <ul>
+              {
+                tech.map(t => {
+                  return <li>{t}</li>
+                })
+              }
+            </ul>
+            <p className="project-text">
+              {about}
+            </p>
+            <div className="git">
+              {!github ? (
+                <p className="project-text">work project -- not shareable</p>
+              ) : (
+                <p>link></p>
+              )}
+            </div>
+          </div>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+        <div className="project-about" style={{position: 'relative', top: '15%'}}>
+          <div className="header subheader project-header">{name}</div>
           <ul>
             {
               tech.map(t => {
@@ -15,10 +41,21 @@ const Project = props => {
               })
             }
           </ul>
-          <p className="project-text">
+          <p className="project-text" style={{width: '70vw'}}>
             {about}
           </p>
+          <div className="git">
+            {!github ? (
+              <p className="project-text">work project -- code not shareable</p>
+            ) : (
+              <p>link></p>
+            )}
+          </div>
         </div>
+        </React.Fragment>
+      )}
+
+
     </div>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,17 +5,25 @@ class Projects extends React.Component {
   state = {
     projects: [
       {
-        name: 'employee language scraper',
-        tech: ['python', 'beautiful soup', 'pandas', 'selenium', 'tkinter'],
-        about: 'The need for language speakers data continued to grow as Analysis Group expanded internationally. We did not have a reporting solution in place to easily gather and report on language data for case staffing or pitches to new clients. Using Selenium and Beautiful Soup to crawl intranet profiles and pandas to build the spreadsheet, I developed a GUI desktop app in tkinter for my teammates to obtain language data easily to assist case managers.'
+        name: 'event app reporting dashboard',
+        tech: ['javascript', 'python', 'bulma', 'bokeh', 'pandas', 'flask', 'react', 'react router', 'react visibility sensor'],
+        about: 'Every year I help support the A/V elements of the annual New Hire Orientation event, including the mobile app, which includes useful information for attendees. The event project leaders wanted usage statistics for a better understanding of the ROI of the app. Unfortunately, reporting on this data was limited, and could only be exported as deeply-nested JSON. I created a data visualization dashboard to present analysis of usage data with Pandas, Bokeh, Flask, React, and Bulma, which helped the event project leaders quickly gain useful insights into how attendees were utilizing the app.',
+        github: null,
+        img: 'ago-app-gif.gif'
       },
       {
         name: 'resume analysis',
-        tech: ['python', 'nltk', 'pandas']
+        tech: ['python', 'nltk', 'pandas'],
+        about: 'In addition to a gap in reporting for language data, case leaders could not easy search employee resumes for past experience or employers. I used natural language processing tools and Pandas to analyze employee resumes for specific keywords, delivering higher quality employee metrics for case staffing and marketing initiatives.',
+        github: null,
+        img: null
       },
       {
-        name: 'event dashboard',
-        tech: ['javascript', 'python', 'bokeh', 'pandas', 'react', 'react router', 'react visibility sensor']
+        name: 'employee language scraper',
+        tech: ['python', 'beautiful soup', 'pandas', 'selenium', 'tkinter'],
+        about: 'The need for language speakers data continued to grow as Analysis Group expanded internationally. We did not have a reporting solution to obtain on language data for case staffing or pitches to new clients. Using Selenium and Beautiful Soup to crawl intranet profiles and pandas to build the spreadsheet, I developed an app to get language proficiency data from 1000+ employee profiles, providing essential data for international marketing initiatives. I also ported this to a GUI in tkinter for my teammates.',
+        github: null,
+        img: null
       },
       {
         name: 'renshu',
@@ -44,7 +52,12 @@ class Projects extends React.Component {
           {this.state.projects.map(project => {
             return (
               <div>
-                <Project name={project.name} tech={project.tech} about={project.about}/>
+                <Project
+                  name={project.name}
+                  tech={project.tech}
+                  about={project.about}
+                  img={project.img}
+                />
               </div>
             )
           })}
