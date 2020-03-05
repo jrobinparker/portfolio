082451d5406e99449e7762d6b433286fbc90db3b
Jeremy Parker <jrobinparker@gmail.com>
2020-03-04 17:39:11 -0800

    Completed nav stylingand toggle animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,37 +2,75 @@ import React from 'react';
 import gsap from 'gsap';
 import { Link } from 'react-router-dom';
 
-const Nav = () => {
-  const expandNav = () => {
-    const tl = gsap.timeline()
-
-    document.getElementById('nav-icon').classList.toggle('change-icon')
-    document.getElementById('nav-bg').classList.toggle('display-bg')
-    document.getElementById('nav-menu').classList.toggle('display-menu')
+class Nav extends React.Component {
+  state = {
+    expand: false
   }
 
-  return (
-    <div className="nav">
-      <div className="nav-icon" id="nav-icon" onClick={expandNav}>
-        <div id="line1" className="line">&nbsp;</div>
-        <div id="line2" className="line">&nbsp;</div>
-        <div id="line3" className="line">&nbsp;</div>
-      </div>
-      <div className="nav-menu" id="nav-menu">
-        <div className="nav-links">
-          <ul className="nav-links-list">
-            <li><Link to={'/'} onClick={expandNav}>home</Link></li>
-            <li><Link to={'/about'} onClick={expandNav}>about me</Link></li>
-            <li><Link to={'/skills'} onClick={expandNav}>skills</Link></li>
-            <li><Link to={'/experience'} onClick={expandNav}>experience</Link></li>
-            <li><Link to={'/projects'} onClick={expandNav}>projects</Link></li>
-          </ul>
+  expandNav() {
+    const menu = document.querySelector('.nav-menu')
+    const icon = document.getElementById('nav-icon')
+    const tl = gsap.timeline()
+    if (!this.state.expand) {
+      document.getElementById('nav-icon').classList.toggle('change-icon')
+      gsap.to(menu, .25, {visibility: 'visible', opacity: 1})
+    } else {
+      document.getElementById('nav-icon').classList.toggle('change-icon')
+      gsap.to(menu, .25, {visibility: 'hidden', opacity: 0})
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="nav">
+        <div className="nav-icon" id="nav-icon" onClick={() => {
+          this.setState({
+            expand: !this.state.expand
+          })
+          this.expandNav()
+        }}>
+          <div id="line1" className="line">&nbsp;</div>
+          <div id="line2" className="line">&nbsp;</div>
+          <div id="line3" className="line">&nbsp;</div>
+        </div>
+        <div className="nav-menu" id="nav-menu">
+          <div className="nav-links">
+              <div className="header"><Link to={'/'} onClick={() => {
+                this.setState({
+                  expand: !this.state.expand
+                })
+                this.expandNav()
+              }}>home</Link></div>
+              <div className="header"><Link to={'/about'} onClick={() => {
+                this.setState({
+                  expand: !this.state.expand
+                })
+                this.expandNav()
+              }}>about me</Link></div>
+              <div className="header" style={{ letterSpacing: '-7px'}}><Link to={'/skills'} onClick={() => {
+                this.setState({
+                  expand: !this.state.expand
+                })
+                this.expandNav()
+              }}>skills</Link></div>
+              <div className="header"><Link to={'/experience'} onClick={() => {
+                this.setState({
+                  expand: !this.state.expand
+                })
+                this.expandNav()
+              }}>experience</Link></div>
+              <div className="header"><Link to={'/projects'} onClick={() => {
+                this.setState({
+                  expand: !this.state.expand
+                })
+                this.expandNav()
+              }}>projects</Link></div>
+          </div>
         </div>
       </div>
-      <div className="nav-bg" id="nav-bg">
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Nav
