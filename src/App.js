f423d45f81927d731570da612b6536dafe124da6
Jeremy Parker <jrobinparker@gmail.com>
2021-01-09 15:23:37 -0800

    Switched to HashRouter to handle page refreshes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
